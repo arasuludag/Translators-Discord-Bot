1a027232c8f91c1bf347c6c8ed90668ba552a728
Aras Uludag <arasuludag@gmail.com>
2022-10-02 19:03:05 +0300

    Add Spanish punctuation marks.

--- functions.js
+++ functions.js
@@ -50,7 +50,7 @@ module.exports = {
   discordStyleProjectName: (project) => {
     const pName = project
       .replace(
-        /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$£€%&()*+,./:;<=>?@[\]^`{|}~]/g,
+        /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$£€%&()*+,./:;<=>?@[\]^`{|}~¡¿]/g,
         ""
       )
       .trim()
